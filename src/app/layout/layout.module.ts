0b0f5d871f3a1a4dd184a87bd629aaf398d684aa
unknown <Administrador@RFR.LOCAL>
2023-06-01 15:55:00 -0500

    Configuraciones iniciales

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
+
+
+@NgModule({
+  declarations: [
+    MainComponent,
+    HeaderComponent,
+    SidenavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutRoutingModule
+  ],
+  exports: [
+    MainComponent,
+    HeaderComponent,
+    SidenavbarComponent
+  ]
+})
+export class LayoutModule { }
