a980d5d515787a2f55e895f2f4aca7da664eb22b
Henry <henry161198@gmail.com>
2023-06-02 02:09:57 -0500

    Terminada estructura de header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  @Output() sideNavToggled = new EventEmitter<boolean>();
+  menuStatus: boolean = false
 
+  ngOnInit(): void {
+    
+  }
+
+  sideNavToggle(){
+    this.menuStatus = !this.menuStatus
+    this.sideNavToggled.emit(this.menuStatus)
+  }
 }
--- src/app/layout/sidenavbar/sidenavbar.component.ts
+++ src/app/layout/sidenavbar/sidenavbar.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidenavbar',
   templateUrl: './sidenavbar.component.html',
   styleUrls: ['./sidenavbar.component.css']
 })
-export class SidenavbarComponent {
+export class SidenavbarComponent implements OnInit {
+  @Input() sideNavStatus: boolean = false
+  
+  list = [
+    {
+      number: '1',
+      name: 'home',
+      icon: 'bi bi-house'
+    },
+    {
+      number: '2',
+      name: 'home',
+      icon: 'bi bi-house'
+    }
+  ]
+
+  ngOnInit(): void {
+    
+  }
+
 
 }
